8b3b195687a6f75e871bb7ba5f960d9715e9cfe5
Denis Filonov <denis@filonov.pro>
2021-02-04 17:04:02 +0100

    Translation fix.

--- lib/generated/l10n.dart
+++ lib/generated/l10n.dart
@@ -212,7 +212,6 @@ class AppLocalizationDelegate extends LocalizationsDelegate<S> {
   List<Locale> get supportedLocales {
     return const <Locale>[
       Locale.fromSubtags(languageCode: 'en'),
-      Locale.fromSubtags(languageCode: 'ru'),
       Locale.fromSubtags(languageCode: 'ru', countryCode: 'RU'),
     ];
   }
