06414fe1a8996d3f79b392e66dc319142a3ac0bc
Denis Filonov <denis@filonov.pro>
2021-02-11 17:38:58 +0100

    Moved about button to BottomBar.

--- lib/pages/routines_page.dart
+++ lib/pages/routines_page.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:npng/config.dart';
 import 'package:npng/services/db.dart';
 import 'package:npng/widgets/multiplatform_widgets.dart';
 import 'package:npng/generated/l10n.dart';
-import 'package:npng/pages/about_page.dart';
-import 'package:package_info_plus/package_info_plus.dart';
 import 'package:npng/widgets/bottom_bar.dart';
 
 class RoutinesPage extends StatefulWidget {
@@ -30,46 +27,14 @@ class _RoutinesPageState extends State<RoutinesPage> {
     setState(() {});
   }
 
-  Future<String> _loadAsset(String path) async {
-    return await rootBundle.loadString(path);
-  }
-
-  /// Preparing data for "About" page
-  void _getAboutPage(BuildContext context) async {
-    Locale myLocale = Localizations.localeOf(context);
-    PackageInfo packageInfo = await PackageInfo.fromPlatform();
-    //String appName = packageInfo.appName;
-    //String packageName = packageInfo.packageName;
-    String version = packageInfo.version;
-    //String buildNumber = packageInfo.buildNumber;
-
-    String about = await _loadAsset("assets/texts/$myLocale/about.md");
-    about = about.replaceAll('%version%', version);
-    String history = await _loadAsset("CHANGELOG.md");
-    Navigator.push(
-      context,
-      mpPageRoute(
-        builder: (context) {
-          return AboutPage(
-            about: about,
-            history: history,
-            version: version,
-          );
-        },
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return MpScaffold(
       appBar: MpAppBar(
         title: Text(S.of(context).pageRoutinesTitle),
         trailing: MpLinkButton(
-          label: S.of(context).about,
-          onPressed: () {
-            _getAboutPage(context);
-          },
+          label: "+",
+          onPressed: () {},
         ),
       ),
       body: Container(
--- lib/widgets/bottom_bar.dart
+++ lib/widgets/bottom_bar.dart
@@ -1,15 +1,49 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:convex_bottom_bar/convex_bottom_bar.dart';
+import 'package:flutter/services.dart';
 import 'package:npng/config.dart';
+import 'package:npng/pages/about_page.dart';
 import 'package:npng/pages/exercises/exercises_page.dart';
 import 'package:npng/generated/l10n.dart';
 import 'package:npng/pages/routines_page.dart';
+import 'package:npng/widgets/multiplatform_widgets.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 class BottomBar extends StatelessWidget {
   BottomBar({this.initialActiveIndex});
 
   final int initialActiveIndex;
+
+  Future<String> _loadAsset(String path) async {
+    return await rootBundle.loadString(path);
+  }
+
+  /// Preparing data for "About" page
+  void _getAboutPage(BuildContext context) async {
+    Locale myLocale = Localizations.localeOf(context);
+    PackageInfo packageInfo = await PackageInfo.fromPlatform();
+    //String appName = packageInfo.appName;
+    //String packageName = packageInfo.packageName;
+    String version = packageInfo.version;
+    //String buildNumber = packageInfo.buildNumber;
+
+    String about = await _loadAsset("assets/texts/$myLocale/about.md");
+    about = about.replaceAll('%version%', version);
+    String history = await _loadAsset("CHANGELOG.md");
+    Navigator.push(
+      context,
+      mpPageRoute(
+        builder: (context) {
+          return AboutPage(
+            about: about,
+            history: history,
+            version: version,
+          );
+        },
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,6 +54,7 @@ class BottomBar extends StatelessWidget {
         TabItem(icon: Icons.calendar_today, title: "Measure"),
         TabItem(icon: Icons.assessment, title: "Stats"),
         TabItem(icon: Icons.ac_unit, title: S.of(context).pageExerciseTitle),
+        TabItem(icon: Icons.help, title: S.of(context).about),
       ],
       initialActiveIndex: this.initialActiveIndex,
       onTap: (int i) {
@@ -31,6 +66,9 @@ class BottomBar extends StatelessWidget {
           case 2:
           case 3:
             Navigator.pushNamed(context, ExercisesPage.id);
+            break;
+          case 4:
+            _getAboutPage(context);
             break;
           default:
         }
