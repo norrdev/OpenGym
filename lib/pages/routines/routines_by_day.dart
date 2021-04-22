2543ec270feb9afac6eef5c907bf904bee1a6804
Denis Filonov <denis@filonov.pro>
2021-04-22 19:15:03 +0200

    Listtile to row

--- lib/pages/routines/routines_by_day.dart
+++ lib/pages/routines/routines_by_day.dart
@@ -1,14 +1,10 @@
-import 'dart:ffi';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:npng/config.dart';
 import 'package:npng/db.dart';
 import 'package:npng/pages/routines/add_excersise.dart';
-import 'package:npng/widgets/modal_popups.dart';
 import 'package:npng/widgets/multiplatform_widgets.dart';
 import 'package:npng/generated/l10n.dart';
-import 'package:npng/widgets/bottom_bar.dart';
 
 class RoutinesByDayPage extends StatefulWidget {
   static String id = 'routines_by_day';
@@ -23,7 +19,6 @@ class RoutinesByDayPage extends StatefulWidget {
 
 class _RoutinesByDayPageState extends State<RoutinesByDayPage> {
   List<Map<String, dynamic>> _results = [];
-  TextEditingController tcSets = TextEditingController(text: '');
 
   @override
   void initState() {
@@ -43,44 +38,55 @@ WHERE days_id = ${widget.dayId};
   @override
   Widget build(BuildContext context) {
     return MpScaffold(
-        appBar: MpAppBar(
-          title: Text(widget.pageTitle),
-          trailing: MpFlatButton(
-            padding: EdgeInsets.all(8),
-            child: Icon(CupertinoIcons.add),
-            onPressed: () => Navigator.push(
-              context,
-              mpPageRoute(
-                builder: (context) {
-                  return AddExcersisePage(dayId: widget.dayId);
-                },
-              ),
-            ).whenComplete(() => _refresh()),
-          ),
-        ),
-        body: Container(
-          constraints: BoxConstraints.expand(),
-          child: SafeArea(
-            child: ListView.builder(
-              itemCount: _results.length,
-              itemBuilder: (context, index) {
-                final item = _results[index];
-                return Material(
-                  type: MaterialType.transparency,
-                  child: Theme(
-                    data: (darkModeOn) ? kMaterialDark : kMaterialLight,
-                    child: ListTile(
-                      title: Text(item['name']),
-                      subtitle: (item['description'] != null)
-                          ? Text(item['description'])
-                          : Text(''),
-                      trailing: CupertinoButton(child: Icon(Icons.delete)),
-                    ),
-                  ),
-                );
+      appBar: MpAppBar(
+        title: Text(widget.pageTitle),
+        trailing: MpFlatButton(
+          padding: EdgeInsets.all(8),
+          child: Icon(CupertinoIcons.add),
+          onPressed: () => Navigator.push(
+            context,
+            mpPageRoute(
+              builder: (context) {
+                return AddExcersisePage(dayId: widget.dayId);
               },
             ),
+          ).whenComplete(() => _refresh()),
+        ),
+      ),
+      body: Container(
+        constraints: BoxConstraints.expand(),
+        child: SafeArea(
+          //ListBuilder(results: _results),
+          child: ListView.builder(
+            itemCount: _results.length,
+            itemBuilder: (context, index) {
+              final item = _results[index];
+              return Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: Row(
+                  children: [
+                    Container(
+                      child: Text(item['name']),
+                      width: MediaQuery.of(context).size.width / 2.0 - 8.0,
+                    ),
+                    Container(
+                      alignment: Alignment.center,
+                      //TODO: Sets and reps editor
+                      child: Text(item['sets'].toString()),
+                      width: MediaQuery.of(context).size.width / 4,
+                    ),
+                    Container(
+                      alignment: Alignment.center,
+                      child: Text(item['repeats'].toString()),
+                      width: MediaQuery.of(context).size.width / 4,
+                    ),
+                  ],
+                ),
+              );
+            },
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
