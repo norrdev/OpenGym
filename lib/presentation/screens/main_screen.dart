8081b69c72b8474d993813efa9babd607b1fffae
Denis Filonov <denis@filonov.pro>
2022-09-01 16:06:35 +0200

    Caption not showed on app launch. Fixes #150

--- lib/presentation/screens/main_screen.dart
+++ lib/presentation/screens/main_screen.dart
@@ -25,126 +25,117 @@ class MainScreen extends StatelessWidget {
     const SettingsScreen()
   ];
 
+  String _title(BuildContext context, int index) {
+    switch (index) {
+      case 0:
+        return S.of(context).pageWorkout;
+      case 1:
+        return S.of(context).pageProgramsTitle;
+      case 2:
+        return S.of(context).pageExerciseTitle;
+      case 3:
+        return S.of(context).log;
+      case 4:
+        return S.of(context).calculate;
+      case 5:
+        return S.of(context).settings;
+      default:
+        return '';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    String title = '';
-
-    return BlocListener<CurrentTabCubit, CurrentTabState>(
-      listener: (context, state) {
-        if (state is CurrentTabLoaded) {
-          switch (state.selectedIndex) {
-            case 0:
-              title = S.of(context).pageWorkout;
-              break;
-            case 1:
-              title = S.of(context).pageProgramsTitle;
-              break;
-            case 2:
-              title = S.of(context).pageExerciseTitle;
-              break;
-            case 3:
-              title = S.of(context).log;
-              break;
-            case 4:
-              title = S.of(context).calculate;
-              break;
-            case 5:
-              title = S.of(context).settings;
-              break;
-            default:
-          }
-        }
-      },
-      child: BlocBuilder<CurrentTabCubit, CurrentTabState>(
-        builder: (context, state) {
-          return Scaffold(
-            drawer: Drawer(
-              child: ListView(
-                children: [
-                  DrawerHeader(
-                    child: Image.asset('assets/icon/icon-alfa.png'),
-                  ),
-                  ListTile(
-                    leading: const Icon(Icons.fitness_center_rounded),
-                    title: Text(S.of(context).pageWorkout),
-                    onTap: () {
-                      context.read<CurrentTabCubit>().saveCurrentIndex(0);
-                      Navigator.pop(context);
-                    },
-                  ),
-                  ListTile(
-                    leading: const Icon(Icons.library_books),
-                    title: Text(S.of(context).pageProgramsTitle),
-                    onTap: () {
-                      context.read<CurrentTabCubit>().saveCurrentIndex(1);
-                      Navigator.pop(context);
-                    },
-                  ),
-                  ListTile(
-                    leading: const Icon(Icons.directions_run_rounded),
-                    title: Text(S.of(context).pageExerciseTitle),
-                    onTap: () {
-                      context.read<CurrentTabCubit>().saveCurrentIndex(2);
-                      Navigator.pop(context);
-                    },
-                  ),
-                  ListTile(
-                    leading: const Icon(Icons.calendar_month_rounded),
-                    title: Text(S.of(context).log),
-                    onTap: () {
-                      context.read<CurrentTabCubit>().saveCurrentIndex(3);
-                      Navigator.pop(context);
-                    },
-                  ),
-                  ListTile(
-                    leading: const Icon(Icons.gas_meter_rounded),
-                    title: Text(S.of(context).calculate),
-                    onTap: () {
-                      context.read<CurrentTabCubit>().saveCurrentIndex(4);
-                      Navigator.pop(context);
-                    },
-                  ),
-                  ListTile(
-                    leading: const Icon(Icons.settings_rounded),
-                    title: Text(S.of(context).settings),
-                    onTap: () {
-                      context.read<CurrentTabCubit>().saveCurrentIndex(5);
-                      Navigator.pop(context);
-                    },
-                  ),
-                ],
-              ),
-            ),
-            appBar: AppBar(
-              title: Text(title),
-              actions: <Widget>[
-                if ((state is CurrentTabLoaded ? state.selectedIndex : 0) == 0)
-                  HelpIconButton(help: S.of(context).hintWorkout),
-                if ((state is CurrentTabLoaded ? state.selectedIndex : 0) == 1)
-                  IconButton(
-                    icon: const Icon(Icons.add),
-                    onPressed: () => Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const ProgramNewScreen(),
-                      ),
-                    ),
-                  ),
-                if ((state is CurrentTabLoaded ? state.selectedIndex : 0) == 1)
-                  HelpIconButton(help: S.of(context).hintPrograms),
-                if ((state is CurrentTabLoaded ? state.selectedIndex : 0) == 2)
-                  HelpIconButton(help: S.of(context).hintEx),
-                if ((state is CurrentTabLoaded ? state.selectedIndex : 0) == 3)
-                  HelpIconButton(help: S.of(context).hintLogCalendar),
+    return BlocBuilder<CurrentTabCubit, CurrentTabState>(
+      builder: (context, state) {
+        return Scaffold(
+          drawer: Drawer(
+            child: ListView(
+              children: [
+                DrawerHeader(
+                  child: Image.asset('assets/icon/icon-alfa.png'),
+                ),
+                ListTile(
+                  leading: const Icon(Icons.fitness_center_rounded),
+                  title: Text(S.of(context).pageWorkout),
+                  onTap: () {
+                    context.read<CurrentTabCubit>().saveCurrentIndex(0);
+                    Navigator.pop(context);
+                  },
+                ),
+                ListTile(
+                  leading: const Icon(Icons.library_books),
+                  title: Text(S.of(context).pageProgramsTitle),
+                  onTap: () {
+                    context.read<CurrentTabCubit>().saveCurrentIndex(1);
+                    Navigator.pop(context);
+                  },
+                ),
+                ListTile(
+                  leading: const Icon(Icons.directions_run_rounded),
+                  title: Text(S.of(context).pageExerciseTitle),
+                  onTap: () {
+                    context.read<CurrentTabCubit>().saveCurrentIndex(2);
+                    Navigator.pop(context);
+                  },
+                ),
+                ListTile(
+                  leading: const Icon(Icons.calendar_month_rounded),
+                  title: Text(S.of(context).log),
+                  onTap: () {
+                    context.read<CurrentTabCubit>().saveCurrentIndex(3);
+                    Navigator.pop(context);
+                  },
+                ),
+                ListTile(
+                  leading: const Icon(Icons.gas_meter_rounded),
+                  title: Text(S.of(context).calculate),
+                  onTap: () {
+                    context.read<CurrentTabCubit>().saveCurrentIndex(4);
+                    Navigator.pop(context);
+                  },
+                ),
+                ListTile(
+                  leading: const Icon(Icons.settings_rounded),
+                  title: Text(S.of(context).settings),
+                  onTap: () {
+                    context.read<CurrentTabCubit>().saveCurrentIndex(5);
+                    Navigator.pop(context);
+                  },
+                ),
               ],
             ),
-            body: IndexedStack(
-              index: state is CurrentTabLoaded ? state.selectedIndex : 0,
-              children: pageList,
-            ),
-          );
-        },
-      ),
+          ),
+          appBar: AppBar(
+            title: Text(_title(
+                context, state is CurrentTabLoaded ? state.selectedIndex : 0)),
+            actions: <Widget>[
+              if ((state is CurrentTabLoaded ? state.selectedIndex : 0) == 0)
+                HelpIconButton(help: S.of(context).hintWorkout),
+              if ((state is CurrentTabLoaded ? state.selectedIndex : 0) == 1)
+                IconButton(
+                  icon: const Icon(Icons.add),
+                  onPressed: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const ProgramNewScreen(),
+                    ),
+                  ),
+                ),
+              if ((state is CurrentTabLoaded ? state.selectedIndex : 0) == 1)
+                HelpIconButton(help: S.of(context).hintPrograms),
+              if ((state is CurrentTabLoaded ? state.selectedIndex : 0) == 2)
+                HelpIconButton(help: S.of(context).hintEx),
+              if ((state is CurrentTabLoaded ? state.selectedIndex : 0) == 3)
+                HelpIconButton(help: S.of(context).hintLogCalendar),
+            ],
+          ),
+          body: IndexedStack(
+            index: state is CurrentTabLoaded ? state.selectedIndex : 0,
+            children: pageList,
+          ),
+        );
+      },
     );
   }
 }
